befcb9e3b76a1f67c7fdf5b9663f491eb55cad0e
DanielPimbor <pimbor.daniel@diak.szbi-pg.hu>
2025-11-26 09:27:52 +0100

    Create Lotto.py

---
+++ Lotto.py
@@ -0,0 +1,8 @@
+print('Köszöntelek az 5-ös lottó játákban.')
+print('Írd le a szelvényed tippjeit ez az üzenet alá, vesszőkkel elválasztva.')
+tipp = input()
+
+import random
+
+for i in range(5):
+    
